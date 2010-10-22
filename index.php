8ce88696edb08334e76df420691cb046daa8990c
Alan Smith <skrat19@gmail.com>
2010-10-22 00:36:18 -0600

    Initial commit; working.

---
+++ index.php
@@ -0,0 +1,74 @@
+<html>
+<head>
+	<title>Skrat's Blog</title>
+
+	<style>
+		h2 {
+			margin-bottom: 3px;
+		}
+
+		p {
+			margin-top: 3px;
+		}
+	</style>
+</head>
+
+<body>
+<?php
+
+ini_set('date.timezone', 'America/New_York');
+
+/*
+ * Start up Acorn
+ *
+ */
+
+define('ROOT_DIR', '.');
+require('acorn.php');
+
+$time = microtime(true);
+
+/*
+ * Get the post(s)
+ *
+ */
+
+if (empty($_GET['id']) === false)
+{
+	$posts = array(Post::postWithId($_GET['id']));
+}
+else
+{
+	$posts = Post::posts();
+}
+
+/*
+ * Loop through the post(s) and diplay 'em
+ *
+ */
+
+foreach ($posts as $post)
+{
+	$link = (isset($_GET['id']) && $_GET['id'] == $post->id) ? $post->title : "<a href='?id={$post->id}'>{$post->title}</a>";
+	$date = date('l, \t\h\e jS \of F, Y', $post->date);
+echo <<<EOT
+<h2>{$link}</h2>
+<p>
+<small>Written on {$date} by {$post->author}.</small>
+<br/>
+{$post->body}
+</p>
+<hr/>
+EOT;
+}
+
+if (isset($_GET['id']))
+{
+	echo "<a href='index.php'>Go back to full listing</a>";
+}
+
+echo '<!--- Page took: '.(microtime(true) - $time).'-->';
+
+?>
+</body>
+</html>
---
+++ post.php
@@ -0,0 +1,59 @@
+<?php
+
+class Post extends AN_Model
+{
+	static function posts()
+	{
+		$posts = array();
+
+		if (is_dir('./posts'))
+		{
+			foreach (glob('posts/*.html') as $p)
+			{
+				$posts[] = self::_post(basename($p));
+			}
+		}
+
+		return $posts;
+	}
+
+	static function postWithId($id)
+	{
+		if (is_dir('./posts') && file_exists("./posts/{$id}.html"))
+		{
+			return self::_post($id.'.html');
+		}
+
+		return null;
+	}
+
+	static function _post($filename)
+	{
+		$matches = array();
+		if (preg_match('/([0-9]{4}-[0-9]{2}-[0-9]{2})-([A-Z|a-z|\-]*?)\.html$/', $filename, $matches))
+		{
+			$file_contents = explode("\n\n", file_get_contents("./posts/{$filename}"));
+			$data = array(
+					'id' => $matches[1].'-'.$matches[2],
+					'date' => strtotime($matches[1]),
+					'title' => ucwords(str_ireplace('-', ' ', $matches[2])),
+					'body' => $file_contents[1]
+			);
+
+			$tags = explode("\n", $file_contents[0]);
+
+			foreach ($tags as $key => $value)
+			{
+				 $split = preg_split('/: ?/', $value);
+
+				 $data[$split[0]] = $split[1];
+			}
+
+			return new self($data);
+		}
+
+		return null;
+	}
+}
+
+?>
