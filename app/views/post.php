d70b4c63978088bbc79e1d24beee29c3cf045e92
Alan Smith <skrat19@gmail.com>
2010-10-26 11:06:48 -0600

    Added Disqus comments to individual post page.

--- app/views/post.php
+++ app/views/post.php
@@ -1,3 +1,15 @@
 <h2><?= $post->title ?></h2>
 <div class='date'>Written on <?= $post->date()?>.</div>
 <?= $post->body() ?>
+
+<div id="disqus_thread"></div>
+<script type="text/javascript">
+  var disqus_identifier = '<?= $post->id ?>';
+  (function() {
+   var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+   dsq.src = 'http://codecabin.disqus.com/embed.js';
+   (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+  })();
+</script>
+<noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript=codecabin">comments powered by Disqus.</a></noscript>
+<a href="http://disqus.com" class="dsq-brlink">blog comments powered by <span class="logo-disqus">Disqus</span></a>
