f39e7bad549fe55bf37b1f8a6226f2e6739cd590
Alan Smith <alan@cloakedcode.com>
2011-03-11 11:19:56 -0700

    Moved piwik so as to discontinue tracking of the Dohnut.

--- app/layout.php
+++ app/layout.php
@@ -56,7 +56,7 @@
 	</script>
 
 	<script type="text/javascript">
-		var pkBaseURL = (("https:" == document.location.protocol) ? "https://cloakedcode.com/piwik/" : "http://cloakedcode.com/piwik/");
+		var pkBaseURL = '/tracker/'; //(("https:" == document.location.protocol) ? "/piwik/" : "/piwik/");
 		document.write(unescape("%3Cscript src='" + pkBaseURL + "piwik.js' type='text/javascript'%3E%3C/script%3E"));
 		</script><script type="text/javascript">
 			try {
@@ -64,7 +64,7 @@
 			piwikTracker.trackPageView();
 		piwikTracker.enableLinkTracking();
 		} catch( err ) {}
-	</script><noscript><p><img src="http://cloakedcode.com/piwik/piwik.php?idsite=1" style="border:0" alt="" /></p></noscript>
+	</script><noscript><p><img src="/tracker/piwik.php?idsite=1" style="border:0" alt="" /></p></noscript>
 	
 </body>
 </html>
