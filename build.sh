20cc75d7afc2c2b775029eaaa3b09bae885470d5
Alan Smith <alan@cloakedcode.com>
2017-01-18 23:43:37 -0800

    Created static site generator for posterity

---
+++ build.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+container='cloakedcode-builder'
+
+docker run -dt --name=$container -p 8080:80 -v $PWD:/var/www/html php:7.0-apache
+
+sleep 2
+
+echo "Index..."
+curl "localhost:8080/index.php" > index.html
+echo
+
+echo "Pages..."
+for f in pages/*.html; do
+    name="$(basename "$f")"
+    echo $name
+    curl "localhost:8080/index.php?page=${name/.html/}" > $name
+done
+echo
+
+echo "Posts..."
+for f in posts/*.html; do
+    name="$(basename "$f")"
+    echo $name
+    curl "localhost:8080/index.php?id=${name/.html/}" > $name
+done
+echo
+
+docker kill $container
+docker rm $container
+
+echo "Done"
